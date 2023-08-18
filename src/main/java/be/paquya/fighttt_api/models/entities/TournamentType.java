50eafe170c8d60674bd589d7d1d444e25171bef2
Antoine Paquay <antoinepaquay64@gmail.com>
2023-08-18 22:41:58 +0200

    Entities

---
+++ src/main/java/be/paquya/fighttt_api/models/entities/TournamentType.java
@@ -0,0 +1,25 @@
+package be.paquya.fighttt_api.models.entities;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Entity
+@NoArgsConstructor @AllArgsConstructor @Builder @ToString @EqualsAndHashCode
+
+public class TournamentType implements Serializable {
+    @Getter
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "TYPE_ID")
+    private Integer id;
+
+    @Getter @Setter
+    @Column(nullable = false, unique = true, name = "NAME", length = 50)
+    private String name;
+
+    @Getter @Setter
+    @Column(nullable = true, name = "DESCRIPTION")
+    private String description;
+}
